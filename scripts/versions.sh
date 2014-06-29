899110cc1b2a46b1a679982efcd7402e9edab73c
hiroyuki nagata <newserver002@gmail.com>
2014-06-29 20:38:58 +0900

    Add autotools for build automation system.

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,15 +2,15 @@
 set -e
 
 # versions of different packages (prefix RUBENVB to prevent conflict with macros)
-export RUBENVB_GCC_VERSION="4.8pre"
+export RUBENVB_GCC_VERSION="4.8.2"
 export RUBENVB_CLANG_VERSION="3.2pre"
 
-export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=rubenvb-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
+export RUBENVB_PACKAGE_OPTIONS="--with-pkgversion=hiroyuki-$RUBENVB_GCC_VERSION --with-bugurl=mingw-w64-public@lists.sourceforge.net"
 
 # this is hiroyuki nagata's library version setting
 export LIBICONV_VERSION="1.14"
 export EXPAT_VERSION="2.1.0"
-export GMP_VERSION="4.3.2"
+export GMP_VERSION="5.1.2"
 export MPFR_VERSION="3.1.2"
 export MPC_VERSION="1.0.1"
 export PPL_VERSION="1.0"
